3d741f466e752691f33ad83bbb8539f587b72f34
nichols20 <johnnytes10@gmail.com>
2021-08-31 12:55:26 -0400

    extending event emitter to logger class

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-//Using thew EventEmitter class we are able to raise and listen to events
-const EventEmitter = require("events");
-
-const emitter = new EventEmitter();
-
-// A listener is a function that will be called when that event is raised. *on/addlistener are both the same
-emitter.on("message Logged", function () {
-  console.log("LISTENER CALLED");
-});
-
-//This function is used to raise an event
-emitter.emit("message Logged");
+const Logger = require("./logger");
+const logger = new Logger();
+logger.log("message");
--- logger.js
+++ logger.js
@@ -1,8 +1,20 @@
+//Using the EventEmitter class we are able to raise and listen to events
+const { EventEmitter } = require("stream");
 
-const url = "http://mylogger.com"
+const url = "http://mylogger.com";
 
-function log(message){
-    console.log(message)
+class Logger extends EventEmitter {
+  log(message) {
+    console.log(message);
+
+    // A listener is a function that will be called when that event is raised. *on/addlistener are both the same
+    //the second arguement is the callback function of the listener. we can pass arguements from the emit function to the listener callback.
+    this.on("message Logged", (arg) => {
+      console.log("LISTENER CALLED", arg);
+    });
+
+    //This function is used to raise an event, the first arguement is event name and the second are the event arguements
+    this.emit("message Logged", { id: 1, url: "http://" });
+  }
 }
-
-module.exports.log = log
+module.exports = Logger;
