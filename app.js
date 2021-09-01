0b53c3ad82425cf2339137498fddcb94f64a5ae9
nichols20 <johnnytes10@gmail.com>
2021-08-31 20:20:17 -0400

    http module practice

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const Logger = require("./logger");
-const logger = new Logger();
-logger.log("message");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello WOrld");
+    res.end();
+  }
+
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});
+
+server.listen(3000);
+
+console.log("listening on port 3000.....");
